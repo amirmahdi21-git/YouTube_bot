0b5f6907666367aaf4d85f4f5857cd02e2dc5583
amirmahdi21-git <amtdyn21git@gmail.com>
2026-07-09 02:43:22 -0700

    fix bugs and add ffmpeg config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
-BOT_TOKEN = os.getenv("8630660599:AAH7-9yr143YtzIT13Tm1KVwziquq8R8gLA")
+BOT_TOKEN = os.getenv("8941196611:AAFaxnZmyITfMTZkf8pa7oXu9IBvmiw2vII")
 DOWNLOAD_PATH = os.getenv("download/")
 os.makedirs(DOWNLOAD_PATH, exist_ok=True)
